2d389714a3790740e9023f029ec9a3d31c1cfe21
malcom.jones <malcom.jones@cnu.edu>
2018-04-07 21:01:56 -0400

    working with new hairtypes-dict.json file

--- json-url-practice.playground/Contents.swift
+++ json-url-practice.playground/Contents.swift
@@ -9,9 +9,7 @@ By default, all top-level code is executed, and then execution is terminated. Wh
  */
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-let url = URL(string: "http://malcomjonesjr.com/json/hairtypes.json")
-let config = URLSessionConfiguration.background(withIdentifier: "json_download_hairtypes")
-let session = URLSession(configuration: config)
+let url = URL(string: "http://malcomjonesjr.com/json/hairtypes-dict.json")
 let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
     if( error != nil) {
         print(error!)
@@ -19,19 +17,37 @@ let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
         if let urlContent = data {
             do {
                 
-                //jsonResult will be a JSON Object in the form of a dictionary [String: AnyObject] - AnyObject will be an array of Hair Types
-                let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options:[]) as! [String: AnyObject]
+                //jsonResult will be a JSON Object in the form of a dictionary [String: Any] - Any will be an array
+                print("DEBUG: pulling down jsonResult")
+                let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options:[]) as! [String: Any]
                 
                 //Extract array of Hair Types
-                let jsonArray = jsonResult["hair"] as! [AnyObject]
-                //print(jsonResult)
-                //print(jsonResult["Nodes"]!!)
+                print("DEBUG: creating straightHair")
                 
-                for i in jsonArray {
-                    let typeName = i["name"]
-                    let typeDesc = i["description"]
-                    print("The hair type \(String(describing: typeName)) is \(String(describing: typeDesc))\n")
-                }
+                // pull hair types from json object
+                let straightHair = jsonResult["straight"] as! [AnyObject]
+                let wavyHair = jsonResult["wavy"] as! [AnyObject]
+                let curlyHair = jsonResult["curly"] as! [AnyObject]
+                let kinkyHair = jsonResult["kinky"] as! [AnyObject]
+                
+                // pull dictionaries from arrays
+                let straightHairDict = straightHair[0] as! [String: Any]
+                let wavyHairDict = wavyHair[0] as! [String: Any]
+                let curlyHairDict = curlyHair[0] as! [String: Any]
+                let kinkyHairDict = kinkyHair[0] as! [String: Any]
+
+                print("DEBUG: pulling names")
+                print(straightHairDict["name"] as! String)
+                print(straightHairDict["description"] as! String)
+                print(wavyHairDict["name"] as! String)
+                print(curlyHairDict["name"] as! String)
+                print(kinkyHairDict["name"] as! String)
+                
+                
+                let wavySub = wavyHairDict["subtype"] as! [AnyObject]
+                let wavySubA = wavySub[0] as! [String: Any]
+                
+                print(wavySubA["name"] as! String)
             } catch {
                 print("JSON processing failed")
             }
